ce7b11853b2254cd710dc6df6c7dd665449d127a
Lantaan <69048952+Lantaan@users.noreply.github.com>
2022-10-05 09:13:58 +0200

    debugging with Andi

--- main.js
+++ main.js
@@ -36,70 +36,71 @@ let foo = computeDistances(coordinates)
 function computeDistances(coordinates){
     //compute Distances between each robot and start point and each robot and exchange point; next_step: retrieving distance estimation from robot as well -> more precise (Update: not possible)
     //Instead we will compute the distances using A* or Dijkstra; issue -> will only output path coordinates not distance (Distance computation can become costly)
-   
+    let a = finder.findPath(coordinates.tuggerY,coordinates.tuggerX, coordinates.startY, coordinates.startX,grid).length;
+    console.log();
     
    
     distances = {
-        tugger_to_start: finder.findPath(coordinates.tuggerX,coordinates.tuggerY, coordinates.startX, coordinates.startY, grid).length * convertConst, //multiply by constant for meters
-        bmw_to_start: finder.findPath(coordinates.bmwX,coordinates.bmwY, coordinates.startX, coordinates.startY, grid).length * convertConst,
-        start_to_exchange: finder.findPath(coordinates.startX,coordinates.startY, coordinates.exchangeX, coordinates.exchangeY, grid).length * convertConst,
-        tugger_to_exchange: finder.findPath(coordinates.tuggerX,coordinates.tuggerY, coordinates.exchangeX, coordinates.exchangeY, grid).length * convertConst,
-        bmw_to_exchange: finder.findPath(coordinates.bmwX,coordinates.bmwY, coordinates.exchangeX, coordinates.exchangeY, grid).length * convertConst,
-        exchange_to_end: finder.findPath(coordinates.exchangeX,coordinates.exchangeY, coordinates.endX, coordinates.endY, grid).length * convertConst
+        tugger_to_start: findDist(coordinates.tuggerX,coordinates.tuggerY, coordinates.startX, coordinates.startY) * convertConst, //multiply by constant for meters
+        bmw_to_start: findDist(coordinates.bmwX,coordinates.bmwY, coordinates.startX, coordinates.startY) * convertConst,
+        start_to_exchange: findDist(coordinates.startX,coordinates.startY, coordinates.exchangeX, coordinates.exchangeY) * convertConst,
+        tugger_to_exchange: findDist(coordinates.tuggerX,coordinates.tuggerY, coordinates.exchangeX, coordinates.exchangeY) * convertConst,
+        bmw_to_exchange: findDist(coordinates.bmwX,coordinates.bmwY, coordinates.exchangeX, coordinates.exchangeY) * convertConst,
+        exchange_to_end: findDist(coordinates.exchangeX,coordinates.exchangeY, coordinates.endX, coordinates.endY) * convertConst
 
        };
 
        return distances;
 }
 
-    
+   
 
- 
- function timeEstimation(distances){
-   //might need unit conversions
-   times = {
-   tugger_to_start:  distances.tugger_to_start / v_tugger_max, //in seconds
-   bmw_to_start:  distances.bmw_to_start / v_bmw_max,
-   start_tugger_to_exchange: distances.start_to_exchange / v_tugger_max, 
-   start_bmw_to_exchange: distances.start_to_exchange / v_bmw_max,
-   tugger_to_exchange:  distances.tugger_to_exchange / v_tugger_max,
-   bmw_to_exchange: distances.bmw_to_exchange / v_bmw_max,
-   exchange_tugger_to_end: distances.exchange_to_end / v_tugger_max,
-   exchange_bmw_to_end: distances.exchange_to_end / v_bmw_max
-   }
-   return times;
- }
- function computeCosts(times){
-   combination1 = times.tugger_to_start + times.start_tugger_to_exchange + times.bmw_to_exchange + times.exchange_bmw_to_end;
-   combination2 = times.bmw_to_start + times.start_bmw_to_exchange + times.tugger_to_exchange + times.exchange_tugger_to_end;
-   return [combination1, combination2];
- }
- 
- function pickVehicle(costs){ //calculating the combinations (2!) of the system and picking the most effective one. Need to know if the vehicle velos are approx. equal
- 
-   if(costs[0] <= costs[1]){
-     console.log("Tugger to pick up delivery from start"); //later on implement return of the time estimation for the process to complete
-     return true;
-      //returns a boolean -> true if tugger is going to start point, false if not (going to exchange directly)
-   }else{
-     console.log("BMW to pick up delivery from start");
-     return false;
-     //bmw goes to start first, tugger to exchange
-   }
- }
- async function computationProcess(initialPos){
-   let distances = computeDistances(initialPos);
-   let times = timeEstimation(distances);
-   let costs = computeCosts(times);
-   let cache = {
-     distances: distances,
-     times: times
-   }
- 
-   return [pickVehicle(costs),cache];
- }
 
-function findDist(x1,y1,x2,y2){
+function timeEstimation(distances){
+  //might need unit conversions
+  times = {
+  tugger_to_start:  distances.tugger_to_start / v_tugger_max, //in seconds
+  bmw_to_start:  distances.bmw_to_start / v_bmw_max,
+  start_tugger_to_exchange: distances.start_to_exchange / v_tugger_max, 
+  start_bmw_to_exchange: distances.start_to_exchange / v_bmw_max,
+  tugger_to_exchange:  distances.tugger_to_exchange / v_tugger_max,
+  bmw_to_exchange: distances.bmw_to_exchange / v_bmw_max,
+  exchange_tugger_to_end: distances.exchange_to_end / v_tugger_max,
+  exchange_bmw_to_end: distances.exchange_to_end / v_bmw_max
+  }
+  return times;
+}
+function computeCosts(times){
+  combination1 = times.tugger_to_start + times.start_tugger_to_exchange + times.bmw_to_exchange + times.exchange_bmw_to_end;
+  combination2 = times.bmw_to_start + times.start_bmw_to_exchange + times.tugger_to_exchange + times.exchange_tugger_to_end;
+  return [combination1, combination2];
+}
+
+function pickVehicle(costs){ //calculating the combinations (2!) of the system and picking the most effective one. Need to know if the vehicle velos are approx. equal
+
+  if(costs[0] <= costs[1]){
+    console.log("Tugger to pick up delivery from start"); //later on implement return of the time estimation for the process to complete
+    return true;
+     //returns a boolean -> true if tugger is going to start point, false if not (going to exchange directly)
+  }else{
+    console.log("BMW to pick up delivery from start");
+    return false;
+    //bmw goes to start first, tugger to exchange
+  }
+}
+async function computationProcess(initialPos){
+  let distances = computeDistances(initialPos);
+  let times = timeEstimation(distances);
+  let costs = computeCosts(times);
+  let cache = {
+    distances: distances,
+    times: times
+  }
+
+  return [pickVehicle(costs),cache];
+}
+
+function findDist(y1,x1,y2,x2,){
         let a = finder.findPath(x1,y1,x2,y2,grid).length;
         console.log(a);
 }
